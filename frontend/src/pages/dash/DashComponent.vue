d4336f0e30a0e038000ba77da2372f42e99708a9
Matheus Lima <matheusbmlima2020@gmai.com>
2021-08-29 11:00:22 -0300

    add frontend folder again

---
+++ frontend/src/pages/dash/DashComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="main">
+        <div class="sidebar">
+            <SideBar/>   
+        </div>
+
+        <div class="content-right">
+
+            <div class="pages">
+                <slot name="slot-pages"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+import SideBar from './components/SideBar.vue';
+//import NavBar from './components/NavBar.vue';
+
+export default {
+    name: 'DashComponent',
+
+    components: {
+        SideBar
+    }
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped>
+
+</style>
